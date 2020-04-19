a44a12e4f5bfd6ab7e8b090ffcfa30c45a749eda
MaticNezic <matic.nezic@celtra.com>
2020-04-19 16:09:41 +0200

    changing postgres server

--- models.py
+++ models.py
@@ -7,8 +7,9 @@ from flask_migrate import Migrate
 
 import json
 
-database_name = "casting_agency"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+# database_name = "casting_agency"
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = "postgres://ciozbcmotosejr:b6422148c963febc7c0fd90b5d7f3947b87fc4a5aa854274276f6cc5727cf942@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/ddin9ki1vl53l5"
 
 db = SQLAlchemy()
 
