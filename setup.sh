d7e1f5aa4ce04ad771a28f7eeee03426233c99af
MaticNezic <matic.nezic@celtra.com>
2020-04-19 23:06:06 +0200

    Added env variables

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 
+export AUTH0_DOMAIN='darkinspiration.eu.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='casting_agency_api'
+export database_path="postgres://ciozbcmotosejr:b6422148c963febc7c0fd90b5d7f3947b87fc4a5aa854274276f6cc5727cf942@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/ddin9ki1vl53l5"
